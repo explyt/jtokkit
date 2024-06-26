50268ea92987149ed358605623fc39e57ddc7920
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2024-06-26 10:31:24 +0200

    :adhesive_bandage: imports

--- lib/src/main/java/com/knuddels/jtokkit/EncodingFactory.java
+++ lib/src/main/java/com/knuddels/jtokkit/EncodingFactory.java
@@ -10,12 +10,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 class EncodingFactory {
     private static final Map<String, Integer> SPECIAL_TOKENS_O200K_BASE;
